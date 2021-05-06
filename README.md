185d869a007c4b7f4f9f9381fba20758bc5eff5e
KangJun <kangjun@yunzhangfang.com>
2021-05-06 13:44:44 +0800

    '添加说明'

--- README.md
+++ README.md
@@ -1,3 +1,25 @@
 # electron-react
 从项目中提取出来的electron基础模板，方便后面再次开发使用
 
+## config
+    main里面放的主进程的配置
+    ui是渲染进程的配置
+
+##  dist 
+    打包生成的目录
+
+##  logs
+    存放日志的地方，用于electron运行查找问题
+
+##  public
+    存放公共js,可以让页面直接在全局引用
+
+##  release
+    生成electron安装包的地方
+
+##  src
+    主进程的代码
+
+##  ui
+    渲染进程的代码
+
--- ui/pages/main/index.tsx
+++ ui/pages/main/index.tsx
@@ -1,29 +1,14 @@
-import { getrandomWrod } from '@ui/services';
-import { Button } from 'antd';
-import React, { useState } from 'react';
-import './index.css';
 import logo from '@ui/assets/logo.png';
 import { ipcRenderer } from 'electron';
+import React, { useState } from 'react';
+import './index.css';
 
 const Main = () => {
-  const [text, setText] = useState<string>('');
-  const getBg = async () => {
-    const data: any = await getrandomWrod();
-    console.log(data);
-    setText(data?.content);
-  };
-
   const goElectronWebsite = () => {
     ipcRenderer.send('open-electron-website');
   };
+
   return (
-    // <div className="main">
-    //   <Button onClick={getBg} style={{ display: 'block', margin: '0 auto' }}>
-    //     随机
-    //   </Button>
-    //   <div style={{ textAlign: 'center', lineHeight: '50px' }}>{text}</div>
-    //   <img className="bg" src="https://api.ghser.com/random/pc.php" alt="" />
-    // </div>
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
